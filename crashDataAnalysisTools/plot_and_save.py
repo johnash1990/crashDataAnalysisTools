d4cac9edba310e8ef43da90de051ec2f1ecc8052
Harry Liu <lhy324@gmail.com>
2016-02-24 20:33:16 -0800

    Update plot_and_save.py based on comments 2

--- crashDataAnalysisTools/plot_and_save.py
+++ crashDataAnalysisTools/plot_and_save.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import numpy as np
 
 from matplotlib.backends.backend_pdf import PdfPages
 
@@ -12,9 +13,10 @@ def plot_x_vs_y(df, colname_x, colname_y):
     @colname_x {string} column name for attribute x
     @colname_y {string} column name for attribute y
     Return:
-    the produced figure
+    @fig {matplotlib.figure} the produced figure
     """
     plt.scatter(df[colname_x], df[colname_y])
+    plt.title(colname_x+' vs. '+colname_x)
     plt.xlabel(colname_x)
     plt.ylabel(colname_y)
     plt.show()
@@ -30,26 +32,25 @@ def plot_model_cdf(rmodel, lowerb, upperb, modelname):
     @upperb {float} the upper bound of the dataset
     @modelname {float} the name of the model, e.g. Probit or Logit
     Return:
-    the produced figure
+    @fig {matplotlib.figure} the produced figure
     """
+
+    # specify the figure size
     fig = plt.figure(figsize=(12, 8))
+
+    # specify the grid parameters encoded as a single integer.
+    # For example, "111" means "1x1 grid, first subplot"
+    # and "234" means "2x3 grid, 4th subplot".
     ax = fig.add_subplot(111)
-    support = np.linspace(-6, 6, 1000)
+
+    # Return 1000 evenly spaced numbers over the specified interval
+    # as the sample points
+    support = np.linspace(lowerb, upperb, 1000)
+
+    # plot the figure, title, labels and legend
     ax.plot(support, rmodel.model.cdf(support), 'r-', label=modelname)
+    ax.title('CDF of' + modelname)
+    ax.xlabel('x')
+    ax.ylabel('CDF')
     ax.legend()
     return fig
-
-
-def save_to_pdf(fig, filename):
-    """
-    Save fig to PDF
-    Parameters:
-    @fig the figure to be saved
-    @filename {string} file name, without suffix
-    Return:
-    the file path
-    """
-    pp = PdfPages(filename + '.pdf')
-    pp.savefig(fig)
-    pp.close()
-    return filename + '.pdf'
