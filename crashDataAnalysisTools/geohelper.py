edb9bd10bf8549458f1fb1a2f29706568e5f8e6e
Harry Liu <lhy324@gmail.com>
2016-03-13 22:45:19 -0700

    geohelper format updated

--- crashDataAnalysisTools/geohelper.py
+++ crashDataAnalysisTools/geohelper.py
@@ -1,51 +1,63 @@
-import geopandas as gp
 import matplotlib.pyplot as plt
+
 from mpl_toolkits.basemap import Basemap
 
 
-def convert_3D_2D(geometry):
-'''
-Takes a GeoSeries of Multi/Polygons and returns a list of Multi/Polygons
-'''
-import geopandas as gp
-new_geo = []
-for p in geometry:
-    if p.has_z:
-        if p.geom_type == 'Polygon':
-            lines = [xy[:2] for xy in list(p.exterior.coords)]
-            new_p = Polygon(lines)
-            new_geo.append(new_p)
-        elif p.geom_type == 'MultiPolygon':
-            new_multi_p = []
-            for ap in p:
-                lines = [xy[:2] for xy in list(ap.exterior.coords)]
-                new_p = Polygon(lines)
-                new_multi_p.append(new_p)
-            new_geo.append(MultiPolygon(new_multi_p))
-return new_geo
-
-
-def draw_road_network_map(shpurl, llon, llat, rlon, rlat)
-
-    map = Basemap(llcrnrlon = llon, llcrnrlat = llat, urcrnrlon = rlon, urcrnrlat = rlat,
-             resolution = 'i', projection = 'tmerc', lat_0 = (llat + rlat) / 2, lon_0 = (llon + rlon) / 2)
-    
+def draw_road_network_map(shpurl, llon, llat, rlon, rlat):
+    '''
+    A function to plot a road network from shapefile
+    Parameters:
+    @shpurl {string} the shapefile url, without suffix,
+    e.g. '../data/highway/wgs84'
+    @llon {float} longitude of the top left corner
+    @llat {float} latitude of the top left corner
+    @rlon {float} longitude of the bottom right corner
+    @rlat {float} latitude of the bottom right corner
+    Return: Nothing
+    '''
+    # set up a map canvas
+    map = Basemap(llcrnrlon=llon, llcrnrlat=llat,
+                  urcrnrlon=rlon, urcrnrlat=rlat, resolution='i',
+                  projection='tmerc', lat_0=(llat+rlat)/2,
+                  lon_0=(llon+rlon)/2)
+    # read the shapefile
     map.readshapefile(shpurl, 'highway')
-    
+    # show the map
     plt.show()
 
-def draw_crash_map(shpurl, name, llon, llat, rlon, rlat, lons, lats, crashrates)
 
-    map = Basemap(llcrnrlon = llon, llcrnrlat = llat, urcrnrlon = rlon, urcrnrlat = rlat,
-             resolution = 'i', projection = 'tmerc', lat_0 = (llat + rlat) / 2, lon_0 = (llon + rlon) / 2)
-
+def draw_crash_map(shpurl, name, llon, llat, rlon, rlat, lons, lats, data):
+    '''
+    A function to plot hot spot map for crash rate and
+    crash severity
+    Parameters:
+    @shpurl {string} the shapefile url, without suffix,
+    e.g. '../data/highway/wgs84'
+    @name {float} either 'rate' or 'severity'
+    @llon {float} longitude of the top left corner
+    @llat {float} latitude of the top left corner
+    @rlon {float} longitude of the bottom right corner
+    @rlat {float} latitude of the bottom right corner
+    @lons {a list of float} longitudes of the crash spots
+    @lats {a list of float} latitudes of the crash spots
+    @data {a list of float} data for crash rates or severity
+    Return: Nothing
+    '''
+    # set up a map canvas
+    map = Basemap(llcrnrlon=llon, llcrnrlat=llat,
+                  urcrnrlon=rlon, urcrnrlat=rlat, resolution='i',
+                  projection='tmerc', lat_0=(llat+rlat)/2,
+                  lon_0=(llon+rlon)/2)
+    # read the shapefile
     map.readshapefile(shpurl, 'highway')
 
-    max = max(crashrates)	
-    for index in range(len(crashrates)):
+    # get the max value from the data to scale the size of the marker
+    max = max(data)
+    # plot the hot spots one by one with the marker
+    # size corresponding to the data
+    for index in range(len(data)):
         x, y = map(lons[index], lats[index])
-        map.plot(x, y, marker= 'o', color = 'r', markersize = crashrates[index] * 25 / max)
-	
-    
+        map.plot(x, y, marker='o', color='r', markersize=(data[index]*25/max))
+
+    # show the map
     plt.show()
-	
